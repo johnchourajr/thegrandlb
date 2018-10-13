aad9d177196996bcd3d137a5c06d3b66cd520b2b
john choura <jchoura@happymoney.com>
2018-10-12 22:45:17 -0700

    fixing build error

--- src/components/PageCarousel.js
+++ src/components/PageCarousel.js
@@ -1,47 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link, { withPrefix } from 'gatsby-link'
-import Flickity from 'react-flickity-component'
 
 // Component
 const PageCarousel = props => {
-  const {
-    className,
-    items,
-    settings,
-  } = props
 
-  const toSlide = (item, i) => {
+  if (typeof window != "undefined") {
+
+    const Flickity = require('react-flickity-component');
+
+    const {
+      className,
+      items,
+      settings,
+    } = props
+
+    const toSlide = (item, i) => {
+      return (
+        <div
+          key={`slide-${i}`}
+          className={`carousel-slide carousel-slide--${i}`}
+          style={{backgroundImage: `url(${item.img})`}}
+        >
+          <p className="caption">{item.caption}</p>
+        </div>
+      )
+    }
+
+    const flickityOptions = {
+      initialIndex: 0,
+      lazyLoad: true,
+      arrowShape: ''
+    }
+
     return (
-      <div
-        key={`slide-${i}`}
-        className={`carousel-slide carousel-slide--${i}`}
-        style={{backgroundImage: `url(${item.img})`}}
+      <Flickity
+        className={'carousel'}
+        elementType={'div'}
+        options={flickityOptions}
+        disableImagesLoaded={false}
+        reloadOnUpdate
       >
-        <p className="caption">{item.caption}</p>
-      </div>
+        {items.map(toSlide)}
+      </Flickity>
     )
-  }
-
-  const flickityOptions = {
-    initialIndex: 0,
-    lazyLoad: true,
-    arrowShape: ''
-  }
-
-  return (
-    <Flickity
-      className={'carousel'}
-      elementType={'div'}
-      options={flickityOptions}
-      disableImagesLoaded={false}
-      reloadOnUpdate
-    >
-      {items.map(toSlide)}
-    </Flickity>
-  )
-
-
+  } else return null
 }
 
 PageCarousel.propTypes = {
