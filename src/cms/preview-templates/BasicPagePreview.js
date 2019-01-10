55265ddf4e8aa271a35c91d5b8593d58890a7041
john choura <jchoura@happymoney.com>
2019-01-10 00:46:54 -0800

    fixing get issue

--- src/cms/preview-templates/BasicPagePreview.js
+++ src/cms/preview-templates/BasicPagePreview.js
@@ -5,7 +5,6 @@ const BasicPagePreview = ({ entry, widgetFor }) => (
   <React.Fragment>
     <h6>{entry.getIn(['data', 'title'])}</h6>
     <h2>{entry.getIn(['data', 'heading'])}</h2>
-    <p>{widgetFor('body')}</p>
   </React.Fragment>
 )
 
