f00f3fef0bdb963e3e9e4c331fa26f27ed72ed21
john choura <jchoura@happymoney.com>
2019-01-24 22:52:22 -0800

    updated CMS content

--- src/data/siteDetails.js
+++ src/data/siteDetails.js
@@ -1,32 +1,5 @@
+import content from './siteDetails.json'
+
 export const siteDetails = {
-  title: "The Grand Long Beach — So Cal’s Premier Event Venue",
-  description: "A premier 40,000 sqft event venue perfect for your wedding, meeting, or any milestone party.",
-  companyName: "Choura Venue Services",
-  phone: "+15624260555",
-  address1: "4101 E. Willow St.",
-  address2: "Long Beach, CA 90815",
-  addressLink: "",
-  url: "https://thegrandlb.com/",
-  email: "dan@grandfandb.com",
-  twitter: "",
-  socialCard: "/img/misc/social_card.png",
-  ga: "UA-131770061-1",
-  sitePublic: false,
-  yelpRating: {
-    title: "Yelp",
-    rating: 4,
-    img: "/img/misc/yelp-logo.svg",
-    link: "https://www.yelp.com/biz/the-grand-long-beach-long-beach",
-  },
-  ratings: [
-    {
-      title: "The Knot",
-      rating: 5,
-      link: "https://www.theknot.com/marketplace/the-grand-long-beach-long-beach-ca-620906",
-    },{
-      title: "WeddingWire",
-      rating: 4.4,
-      link: "https://www.weddingwire.com/biz/the-grand-long-beach-long-beach/05b58faf6174714e.html",
-    },
-  ]
+  ...content
 }
