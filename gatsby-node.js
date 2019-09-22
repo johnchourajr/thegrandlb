8864e9264be7cb57cfaa8dadc8d278aa1ed04d23
john choura <hi@john.design>
2019-09-22 00:13:24 -1000

    fixing build issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -65,18 +65,3 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
-
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: 'react-flickity-component',
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
