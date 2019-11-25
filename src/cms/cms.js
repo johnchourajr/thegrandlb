c036afefdef7370b5d4fe4a596d615777cd400a0
john choura <hi@john.design>
2019-11-25 14:04:16 -0800

    adding back preview styles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,17 +8,17 @@ import SiteDetailsPreview from './preview-templates/SiteDetailsPreview'
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-// CMS.registerPreviewStyle('/styles.css')
+CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('siteDetails', SiteDetailsPreview)
-// CMS.registerPreviewTemplate('tourPages', BasicPagePreview)
-//
-// CMS.registerPreviewTemplate('basics', BasicPagePreview)
-// CMS.registerPreviewTemplate('basicsAppetizers', BasicPagePreview)
-// CMS.registerPreviewTemplate('basicsMains', BasicPagePreview)
-// CMS.registerPreviewTemplate('basicsMainsShared', BasicPagePreview)
-// CMS.registerPreviewTemplate('basicsSweets', BasicPagePreview)
-// CMS.registerPreviewTemplate('basicsDrinks', BasicPagePreview)
-// CMS.registerPreviewTemplate('classicMenu', BasicPagePreview)
-// CMS.registerPreviewTemplate('corporateMenu', BasicPagePreview)
-// CMS.registerPreviewTemplate('milestoneMenu', BasicPagePreview)
-// CMS.registerPreviewTemplate('weddingMenu', BasicPagePreview)
+CMS.registerPreviewTemplate('tourPages', BasicPagePreview)
+
+CMS.registerPreviewTemplate('basics', BasicPagePreview)
+CMS.registerPreviewTemplate('basicsAppetizers', BasicPagePreview)
+CMS.registerPreviewTemplate('basicsMains', BasicPagePreview)
+CMS.registerPreviewTemplate('basicsMainsShared', BasicPagePreview)
+CMS.registerPreviewTemplate('basicsSweets', BasicPagePreview)
+CMS.registerPreviewTemplate('basicsDrinks', BasicPagePreview)
+CMS.registerPreviewTemplate('classicMenu', BasicPagePreview)
+CMS.registerPreviewTemplate('corporateMenu', BasicPagePreview)
+CMS.registerPreviewTemplate('milestoneMenu', BasicPagePreview)
+CMS.registerPreviewTemplate('weddingMenu', BasicPagePreview)
