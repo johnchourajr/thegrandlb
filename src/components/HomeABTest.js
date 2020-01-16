876bf09d5b1392acec971bdb09909b8e864edf83
john choura <hi@john.design>
2020-01-15 22:05:05 -0800

    adding site banner

--- src/components/HomeABTest.js
+++ src/components/HomeABTest.js
@@ -1,42 +1,39 @@
-import React from 'react'
+import React from "react";
 
 // **
 // ** Using React GA for event tracking
 // https://github.com/pushtell/react-ga
-import ReactGA from 'react-ga'
+import ReactGA from "react-ga";
 
 // **
 // ** Using React AB Test to conduct the AB Testing
 // https://github.com/pushtell/react-ab-test
-import Experiment from "react-ab-test/lib/Experiment"
-import Variant from "react-ab-test/lib/Variant"
-import emitter from "react-ab-test/lib/emitter"
+import Experiment from "react-ab-test/lib/Experiment";
+import Variant from "react-ab-test/lib/Variant";
+import emitter from "react-ab-test/lib/emitter";
 
-import PageCta from '../components/PageCta'
-import ScrollAnimate from '../components/ScrollAnimate'
-import PromoSection from "../components/PromoSection"
-
+import PageCta from "../components/PageCta";
+import ScrollAnimate from "../components/ScrollAnimate";
+import PromoSection from "../components/PromoSection";
 
 class HomeABTest extends React.Component {
-
-  render () {
-    
+  render() {
     return (
       <ScrollAnimate>
         <Experiment ref="experiment" name="Test092919__Home">
           <Variant name="A">
             <PageCta
-              accent={'grandPink'}
+              accent={"grandPink"}
               heading={`Chat with our\nEvent Pro's today`}
               buttons={[
                 {
                   text: "Let's Talk",
                   url: "/contact/",
                   event: {
-                    category: 'ContactAction',
-                    action: 'Test092919__Home__Action_A',
-                    label: 'Test092919__Home__Action_A',
-                  },
+                    category: "ContactAction",
+                    action: "Test092919__Home__Action_A",
+                    label: "Test092919__Home__Action_A"
+                  }
                 }
               ]}
               img={"/img/about/team-sales.jpg"}
@@ -44,51 +41,49 @@ class HomeABTest extends React.Component {
           </Variant>
           <Variant name="B">
             <PageCta
-              accent={'grandPink'}
+              accent={"grandPink"}
               heading={"Chat with Elizabeth\nor Marissa today."}
               buttons={[
                 {
                   text: "Let's Talk",
                   url: "/contact/",
                   event: {
-                    category: 'ContactAction',
-                    action: 'Test092919__Home__Action_B',
-                    label: 'Test092919__Home__Action_B',
-                  },
+                    category: "ContactAction",
+                    action: "Test092919__Home__Action_B",
+                    label: "Test092919__Home__Action_B"
+                  }
                 }
               ]}
               img={"/img/about/team-elizabeth-melissa-2.jpg"}
             />
           </Variant>
-          { /*
-            <Variant name="C">
-              <PromoSection siteBanner={this.props.siteBanner} />
-            </Variant>
-          */ }
+          <Variant name="C">
+            <PromoSection siteBanner={this.props.siteBanner} />
+          </Variant>
         </Experiment>
       </ScrollAnimate>
-    )
+    );
   }
 }
 
 // Called when the experiment is displayed to the user.
 emitter.addPlayListener((experimentName, variantName) => {
-  const str = `Displaying experiment '${experimentName}' variant '${variantName}'`
+  const str = `Displaying experiment '${experimentName}' variant '${variantName}'`;
 
   ReactGA.event({
-    category: 'Test092919__Home',
-    action: str,
+    category: "Test092919__Home",
+    action: str
   });
 });
 
 // Called when a 'win' is emitted, in this case by this.refs.experiment.win()
 emitter.addWinListener((experimentName, variantName) => {
-  const str = `Variant '${variantName}' of experiment '${experimentName}' was clicked`
+  const str = `Variant '${variantName}' of experiment '${experimentName}' was clicked`;
 
   ReactGA.event({
-    category: 'Test092919__Home',
-    action: str,
+    category: "Test092919__Home",
+    action: str
   });
 });
 
-export default HomeABTest
+export default HomeABTest;
--- src/components/PromoSection.js
+++ src/components/PromoSection.js
@@ -1,44 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
 
 // Components
-import PageSection from '../components/PageSection'
-import Buttons from '../components/Buttons'
-
+import PageSection from "../components/PageSection";
+import Buttons from "../components/Buttons";
+import BannerModal from "./BannerModal";
 
 const PromoSection = props => {
+  const [modalVisible, handleModal] = useState(false);
+
   return (
     <PageSection sectionClassName="xs-pt5">
       <div className="page-promo--wrapper">
         <div className="page-promo--left">
-          <img src="/img/promo/01-friday-sunday-graphic.jpg" alt="Friday & Sunday Promo"/>
+          <img
+            src="/img/promo/01-friday-sunday-graphic.jpg"
+            alt="Friday & Sunday Promo"
+          />
         </div>
         <div className="page-promo--right">
           <div className="page-promo--right__text">
-            <h6>Friday Events</h6>
+            <h6>Our Specials</h6>
+            <h1>Thursday is the New Friday</h1>
             <h1>Free Chivari Chair Fridays</h1>
+            <h1>Sunday Funday</h1>
           </div>
           <Buttons
             buttons={[
               {
                 text: props.siteBanner.modalDetail.buttonText,
                 url: props.siteBanner.modalDetail.buttonUrl,
-                isSecondary: true,
+                isSecondary: false,
                 event: {
-                  category: 'ContactAction',
-                  action: 'Test092919__Home__Action_C',
-                  label: 'Test092919__Home__Action_C',
-                },
+                  category: "ContactAction",
+                  action: "Test092919__Home__Action_C",
+                  label: "Test092919__Home__Action_C"
+                }
               }
             ]}
           />
-          <div className="page-promo--right__text">
-            <h6>Sunday Events</h6>
-            <h1>5% off all Appetizers</h1>
+          <div
+            className="page-promo--right__button"
+            onClick={e => handleModal(true)}
+          >
+            Learn More
           </div>
         </div>
       </div>
+      <BannerModal
+        modalVisible={modalVisible}
+        handleModal={handleModal}
+        modalDetail={props.siteBanner.modalDetail}
+      />
     </PageSection>
-  )
-}
+  );
+};
 
-export default PromoSection
+export default PromoSection;
