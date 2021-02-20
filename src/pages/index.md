ced606f56ba9addfcea0ad3b6b5c21921f6d1b8b
John Choura <jchoura@godaddy.com>
2021-02-20 13:11:56 -0800

    formatting

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,144 +1,142 @@
 ---
-templateKey: 'index-page'
+templateKey: "index-page"
 path: /
 title: The Grand
 heading: |
-  So Cal’s *Premier*
-  Event Venue
+    So Cal’s *Premier*
+    Event Venue
 headingButtons:
-- text: "Get a Quote"
-  url: "/inquire/"
-- text: "Take a Tour"
-  url: "/tour/"
-  isSecondary: true
+    - text: "Get a Quote"
+      url: "/inquire/"
+    - text: "Take a Tour"
+      url: "/tour/"
+      isSecondary: true
 subhead: |
-  40,000 sqft. of
-  Whatever You Want
+    40,000 sqft. of
+    Whatever You Want
 topFeatures:
-- text: |
-    Meetings in
-    Grand Style
-  url: "/events/business/"
-- text: |
-    New in
-    Our Kitchen
-  url: "/menus/"
-- text: |
-    Tour Our
-    Facility
-  url: "/tour/"
+    - text: |
+          Meetings in
+          Grand Style
+      url: "/events/business/"
+    - text: |
+          New in
+          Our Kitchen
+      url: "/menus/"
+    - text: |
+          Tour Our
+          Facility
+      url: "/tour/"
 ctaUpper:
-  img: "/img/moments/party.jpg"
-  accent: "grandBabyBlue"
-  heading: |
-    Plan Your
-    Next Event
-  buttons:
-  - text: "Plan Your Private Event"
-    url: "/inquire/"
-    event:
-      category: InquireAction
-      action: homeCtaUpperInquireAction
+    img: "/img/moments/party.jpg"
+    accent: "grandBabyBlue"
+    heading: |
+        Plan Your
+        Next Event
+    buttons:
+        - text: "Plan Your Private Event"
+          url: "/inquire/"
+          event:
+              category: InquireAction
+              action: homeCtaUpperInquireAction
 map:
-  heading: |
-    So. Cal.
-    Native
-  img: "/img/placeholder--map.jpg"
-  features:
-  - text: "5 Minutes from \nLong Beach Airport"
-    url: "https://www.google.com/maps/dir/Long+Beach+Airport,+Donald+Douglas+Dr,+Long+Beach,+CA/thegrandlb/@33.8111233,-118.1501993,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x80dd3236c1430c85:0x3d72b50085627ff!2m2!1d-118.1523845!2d33.8176974!1m5!1m1!1s0x80dd3187fe1d4d89:0x28bb151385838a33!2m2!1d-118.1473096!2d33.8041368!3e0"
-    event:
-      category: "MapFeatureAction"
-      action: "MapFeatureLGB"
-  - text: "Walking Distance to\nHotel Accommodations"
-    url: "https://www.google.com/maps/dir/Residence+Inn+by+Marriott+Long+Beach,+East+Willow+Street,+Long+Beach,+CA/thegrandlb/@33.8038015,-118.1464431,19z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x80dd318836554d57:0x13034432a0b5caa8!2m2!1d-118.1448683!2d33.803945!1m5!1m1!1s0x80dd3187fe1d4d89:0x28bb151385838a33!2m2!1d-118.1473096!2d33.8041368!3e2"
-    event:
-      category: "MapFeatureAction"
-      action: "MapFeatureHotel"
-  - text: "20 Minutes from\n LAX Airport"
-    url: "https://www.google.com/maps/dir/LAX+airport+(LAX),+World+Way,+Los+Angeles,+CA/thegrandlb/@33.8746301,-118.3446163,12z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x80c2b0d213b24fb5:0x77a87b57698badf1!2m2!1d-118.40853!2d33.9415889!1m5!1m1!1s0x80dd3187fe1d4d89:0x28bb151385838a33!2m2!1d-118.1473096!2d33.8041368!3e0"
-    event:
-      category: "MapFeatureAction"
-      action: "MapFeatureLAX"
+    heading: |
+        So. Cal.
+        Native
+    img: "/img/placeholder--map.jpg"
+    features:
+        - text: "5 Minutes from\nLong Beach Airport"
+          url: "https://www.google.com/maps/dir/Long+Beach+Airport,+Donald+Douglas+Dr,+Long+Beach,+CA/thegrandlb/@33.8111233,-118.1501993,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x80dd3236c1430c85:0x3d72b50085627ff!2m2!1d-118.1523845!2d33.8176974!1m5!1m1!1s0x80dd3187fe1d4d89:0x28bb151385838a33!2m2!1d-118.1473096!2d33.8041368!3e0"
+          event:
+              category: "MapFeatureAction"
+              action: "MapFeatureLGB"
+        - text: "Walking Distance to\nHotel Accommodations"
+          url: "https://www.google.com/maps/dir/Residence+Inn+by+Marriott+Long+Beach,+East+Willow+Street,+Long+Beach,+CA/thegrandlb/@33.8038015,-118.1464431,19z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x80dd318836554d57:0x13034432a0b5caa8!2m2!1d-118.1448683!2d33.803945!1m5!1m1!1s0x80dd3187fe1d4d89:0x28bb151385838a33!2m2!1d-118.1473096!2d33.8041368!3e2"
+          event:
+              category: "MapFeatureAction"
+              action: "MapFeatureHotel"
+        - text: "20 Minutes from\nLAX Airport"
+          url: "https://www.google.com/maps/dir/LAX+airport+(LAX),+World+Way,+Los+Angeles,+CA/thegrandlb/@33.8746301,-118.3446163,12z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x80c2b0d213b24fb5:0x77a87b57698badf1!2m2!1d-118.40853!2d33.9415889!1m5!1m1!1s0x80dd3187fe1d4d89:0x28bb151385838a33!2m2!1d-118.1473096!2d33.8041368!3e0"
+          event:
+              category: "MapFeatureAction"
+              action: "MapFeatureLAX"
 carousel:
-  array:
-  - img: "/img/tour-spaces/GLB_Details_Spaces_2.jpg"
-    caption: "[Palm Terrace](/tour/palm-terrace) with Lounge Furniture"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_3.jpg"
-    caption: "[Catalina Room](/tour/catalina-room) with Chivari Chairs"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_4.jpg"
-    caption: "A [Palm Terrace](/tour/palm-terrace) Table Setting"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_5.jpg"
-    caption: "[Monarch Room Patio](/tour/monarch-room) Ceremony"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_6.jpg"
-    caption: "[Grand Ballroom](/tour/grand-ballroom) with Gold Chivari Chairs"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_7.jpg"
-    caption: "Large [Board Room](/tour/board-room) Table"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_8.jpg"
-    caption: "Champagne Glassware"
-  - img: "/img/tour-spaces/GLB_Details_Spaces_10.jpg"
-    caption: "[Pacific Room](/tour/pacific-room) Table Setting"
+    array:
+        - img: "/img/tour-spaces/GLB_Details_Spaces_2.jpg"
+          caption: "[Palm Terrace](/tour/palm-terrace) with Lounge Furniture"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_3.jpg"
+          caption: "[Catalina Room](/tour/catalina-room) with Chivari Chairs"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_4.jpg"
+          caption: "A [Palm Terrace](/tour/palm-terrace) Table Setting"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_5.jpg"
+          caption: "[Monarch Room Patio](/tour/monarch-room) Ceremony"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_6.jpg"
+          caption: "[Grand Ballroom](/tour/grand-ballroom) with Gold Chivari Chairs"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_7.jpg"
+          caption: "Large [Board Room](/tour/board-room) Table"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_8.jpg"
+          caption: "Champagne Glassware"
+        - img: "/img/tour-spaces/GLB_Details_Spaces_10.jpg"
+          caption: "[Pacific Room](/tour/pacific-room) Table Setting"
 numbers:
-  heading: |
-    Grand by
-    the Numbers
-  array:
-  - prefix: ""
-    number: '11'
-    suffix: ""
-    caption: "Luscious Acres"
-  - prefix: ""
-    number: "40"
-    suffix: "k"
-    caption: "Square Foot Facility"
-  - prefix: ""
-    number: "6"
-    suffix: ""
-    caption: "Flexible Indoor Spaces"
-  - prefix: ""
-    number: "2"
-    suffix: ""
-    caption: "Room-Adjacent Patios"
-  - prefix: ""
-    number: "2"
-    suffix: ""
-    caption: "Outdoor Terraces"
-  - prefix: ""
-    number: "2"
-    suffix: "k"
-    caption: "Total Number of Guests"
-  - prefix: ""
-    number: "400"
-    suffix: ""
-    caption: "Complimentary Parking Spots"
-  - prefix: ""
-    number: "1"
-    suffix: ""
-    caption: "World Class Kitchen"
+    heading: |
+        Grand by
+        the Numbers
+    array:
+        - prefix: ""
+          number: "11"
+          suffix: ""
+          caption: "Luscious Acres"
+        - prefix: ""
+          number: "40"
+          suffix: "k"
+          caption: "Square Foot Facility"
+        - prefix: ""
+          number: "6"
+          suffix: ""
+          caption: "Flexible Indoor Spaces"
+        - prefix: ""
+          number: "2"
+          suffix: ""
+          caption: "Room-Adjacent Patios"
+        - prefix: ""
+          number: "2"
+          suffix: ""
+          caption: "Outdoor Terraces"
+        - prefix: ""
+          number: "2"
+          suffix: "k"
+          caption: "Total Number of Guests"
+        - prefix: ""
+          number: "400"
+          suffix: ""
+          caption: "Complimentary Parking Spots"
+        - prefix: ""
+          number: "1"
+          suffix: ""
+          caption: "World Class Kitchen"
 ctaLower:
-  img: "/img/moments/party-2.jpg"
-  accent: "grandPink"
-  heading: |
-    Plan Your
-    Next Event
-  buttons:
-  - text: "Plan Your Private Event"
-    url: "/inquire/"
-    event:
-      category: InquireAction
-      action: homeCtaLowerInquireAction
+    img: "/img/moments/party-2.jpg"
+    accent: "grandPink"
+    heading: |
+        Plan Your
+        Next Event
+    buttons:
+        - text: "Plan Your Private Event"
+          url: "/inquire/"
+          event:
+              category: InquireAction
+              action: homeCtaLowerInquireAction
 menuFeature:
-  heading: |
-    Food & Drink
-    Es Bueno
-  buttons:
-  - text: "See The Menus"
-    url: "/menus/"
-    isSecondary: true
-  imgs:
-  - "/img/menus.jpg"
-  - "/img/food.jpg"
-  - "/img/menus/5.jpg"
-
-
+    heading: |
+        Food & Drink
+        Es Bueno
+    buttons:
+        - text: "See The Menus"
+          url: "/menus/"
+          isSecondary: true
+    imgs:
+        - "/img/menus.jpg"
+        - "/img/food.jpg"
+        - "/img/menus/5.jpg"
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -24,215 +24,231 @@ import * as util from "../components/functions/util";
 
 // Page
 const IndexPage = ({ data, status, siteBanner }) => {
-  const { frontmatter } = data.markdownRemark;
+	const { frontmatter } = data.markdownRemark;
 
-  return (
-    <Layout status={status}>
-      <PageHero
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        video={{
-          source: [
-            { src: "/video/home.compressed.mp4", type: "video/mp4" },
-            { src: "/video/home.compressed.ogv", type: "video/ogv" },
-            { src: "/video/home.compressed.webm", type: "video/webm" }
-          ],
-          poster: "/video/home-poster.jpg"
-        }}
-        img="/img/placeholder--front-img.jpg"
-        buttons={[
-          {
-            text: "Get a Quote",
-            url: "/inquire/",
-            event: {
-              category: "InquireAction",
-              action: "homeInquireAction"
-            }
-          },
-          {
-            text: "Take a Tour",
-            url: "/tour/",
-            event: {
-              category: "TourAction",
-              action: "homeTourAction"
-            },
-            isSecondary: true
-          }
-        ]}
-      />
-      <PageSection heading={frontmatter.subhead} />
+	return (
+		<Layout status={status}>
+			<PageHero
+				title={frontmatter.title}
+				heading={frontmatter.heading}
+				video={{
+					source: [
+						{
+							src: "/video/home.compressed.mp4",
+							type: "video/mp4",
+						},
+						{
+							src: "/video/home.compressed.ogv",
+							type: "video/ogv",
+						},
+						{
+							src: "/video/home.compressed.webm",
+							type: "video/webm",
+						},
+					],
+					poster: "/video/home-poster.jpg",
+				}}
+				img="/img/placeholder--front-img.jpg"
+				buttons={[
+					{
+						text: "Get a Quote",
+						url: "/inquire/",
+						event: {
+							category: "InquireAction",
+							action: "homeInquireAction",
+						},
+					},
+					{
+						text: "Take a Tour",
+						url: "/tour/",
+						event: {
+							category: "TourAction",
+							action: "homeTourAction",
+						},
+						isSecondary: true,
+					},
+				]}
+			/>
+			<PageSection heading={frontmatter.subhead} />
 
-      <HomeABTest siteBanner={siteBanner} frontmatter={frontmatter} />
+			<HomeABTest siteBanner={siteBanner} frontmatter={frontmatter} />
 
-      <PageSection wrapperClassName="page-feature--wrapper">
-        {frontmatter.topFeatures.map((item, i) => {
-          return (
-            <ScrollAnimate
-              delay={150 * i}
-              key={i}
-              className="page-feature page-feature--lower-bar col xs-col-12 md-col-4"
-            >
-              <a href={item.url} target="_blank" rel="noopener noreferrer">
-                <h1 className="xs-text-center">{item.text}</h1>
-              </a>
-            </ScrollAnimate>
-          );
-        })}
-      </PageSection>
+			<PageSection wrapperClassName="page-feature--wrapper">
+				{frontmatter.topFeatures.map((item, i) => {
+					return (
+						<ScrollAnimate
+							delay={150 * i}
+							key={i}
+							className="page-feature page-feature--lower-bar col xs-col-12 md-col-4"
+						>
+							<a
+								href={item.url}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<h1 className="xs-text-center">{item.text}</h1>
+							</a>
+						</ScrollAnimate>
+					);
+				})}
+			</PageSection>
 
-      <MapSection
-        heading={frontmatter.map.heading}
-        mapLink={util.addressLink(siteDetails.address1, siteDetails.address2)}
-        address1={siteDetails.address1}
-        address2={siteDetails.address2}
-        features={frontmatter.map.features}
-      />
-      <PageSection>
-        <PageCarousel
-          items={[...frontmatter.carousel.array]}
-          settings={{
-            showIndicators: true,
-            infiniteLoop: true,
-            emulateTouch: true
-          }}
-        />
-      </PageSection>
-      <NumberArray
-        heading={frontmatter.numbers.heading}
-        array={frontmatter.numbers.array}
-      />
-      <PageSection sectionClassName="section--flush-bottom">
-        <RatingsSection />
-      </PageSection>
-      <PageSection sectionClassName="section--flush-top">
-        <ReviewsSection />
-      </PageSection>
-      <PageCta
-        heading={frontmatter.ctaLower.heading}
-        buttons={frontmatter.ctaLower.buttons}
-        img={frontmatter.ctaLower.img}
-      />
-      <PageSection
-        heading={frontmatter.menuFeature.heading}
-        headingClassName={"xs-mb3"}
-        buttons={frontmatter.menuFeature.buttons}
-        disabledAnimation
-      >
-        <div className="menu-feature--wrap gutters clearfix">
-          <ScrollAnimate
-            className={`col xs-offset-1 xs-col-10 md-ml0 md-col-7 xs-my2 menu-feature--1`}
-          >
-            <img
-              src={frontmatter.menuFeature.imgs[0]}
-              alt="food"
-              title="food"
-            />
-          </ScrollAnimate>
-          <ScrollAnimate
-            className={`col xs-offset-1 xs-col-10 md-ml0 md-col-5 xs-my2 menu-feature--2`}
-          >
-            <img
-              src={frontmatter.menuFeature.imgs[1]}
-              alt="food"
-              title="food"
-            />
-          </ScrollAnimate>
-          <ScrollAnimate
-            className={`col xs-offset-1 xs-col-10 md-offset-1 md-col-6 lg-offset-3 lg-col-4 xs-my2 menu-feature--3`}
-          >
-            <img
-              src={frontmatter.menuFeature.imgs[2]}
-              alt="food"
-              title="food"
-            />
-          </ScrollAnimate>
-        </div>
-      </PageSection>
-    </Layout>
-  );
+			<MapSection
+				heading={frontmatter.map.heading}
+				mapLink={util.addressLink(
+					siteDetails.address1,
+					siteDetails.address2
+				)}
+				address1={siteDetails.address1}
+				address2={siteDetails.address2}
+				features={frontmatter.map.features}
+			/>
+			<PageSection>
+				<PageCarousel
+					items={[...frontmatter.carousel.array]}
+					settings={{
+						showIndicators: true,
+						infiniteLoop: true,
+						emulateTouch: true,
+					}}
+				/>
+			</PageSection>
+			<NumberArray
+				heading={frontmatter.numbers.heading}
+				array={frontmatter.numbers.array}
+			/>
+			<PageSection sectionClassName="section--flush-bottom">
+				<RatingsSection />
+			</PageSection>
+			<PageSection sectionClassName="section--flush-top">
+				<ReviewsSection />
+			</PageSection>
+			<PageCta
+				heading={frontmatter.ctaLower.heading}
+				buttons={frontmatter.ctaLower.buttons}
+				img={frontmatter.ctaLower.img}
+			/>
+			<PageSection
+				heading={frontmatter.menuFeature.heading}
+				headingClassName={"xs-mb3"}
+				buttons={frontmatter.menuFeature.buttons}
+				disabledAnimation
+			>
+				<div className="menu-feature--wrap gutters clearfix">
+					<ScrollAnimate
+						className={`col xs-offset-1 xs-col-10 md-ml0 md-col-7 xs-my2 menu-feature--1`}
+					>
+						<img
+							src={frontmatter.menuFeature.imgs[0]}
+							alt="food"
+							title="food"
+						/>
+					</ScrollAnimate>
+					<ScrollAnimate
+						className={`col xs-offset-1 xs-col-10 md-ml0 md-col-5 xs-my2 menu-feature--2`}
+					>
+						<img
+							src={frontmatter.menuFeature.imgs[1]}
+							alt="food"
+							title="food"
+						/>
+					</ScrollAnimate>
+					<ScrollAnimate
+						className={`col xs-offset-1 xs-col-10 md-offset-1 md-col-6 lg-offset-3 lg-col-4 xs-my2 menu-feature--3`}
+					>
+						<img
+							src={frontmatter.menuFeature.imgs[2]}
+							alt="food"
+							title="food"
+						/>
+					</ScrollAnimate>
+				</div>
+			</PageSection>
+		</Layout>
+	);
 };
 
 export default IndexPage;
 
 export const basicPageQuery = graphql`
-  query IndexPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-        heading
-        headingButtons {
-          text
-          url
-          isSecondary
-        }
-        subhead
-        topFeatures {
-          text
-          url
-        }
-        ctaUpper {
-          img
-          accent
-          heading
-          buttons {
-            text
-            url
-            event {
-              category
-              action
-            }
-          }
-        }
-        map {
-          heading
-          features {
-            text
-            url
-            event {
-              category
-              action
-            }
-          }
-        }
-        carousel {
-          array {
-            img
-            caption
-          }
-        }
-        numbers {
-          heading
-          array {
-            prefix
-            number
-            suffix
-            caption
-          }
-        }
-        ctaLower {
-          img
-          accent
-          heading
-          buttons {
-            text
-            url
-            event {
-              category
-              action
-            }
-          }
-        }
-        menuFeature {
-          heading
-          buttons {
-            text
-            url
-            isSecondary
-          }
-          imgs
-        }
-      }
-    }
-  }
+	query IndexPage($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			html
+			frontmatter {
+				title
+				heading
+				headingButtons {
+					text
+					url
+					isSecondary
+				}
+				subhead
+				topFeatures {
+					text
+					url
+				}
+				ctaUpper {
+					img
+					accent
+					heading
+					buttons {
+						text
+						url
+						event {
+							category
+							action
+						}
+					}
+				}
+				map {
+					heading
+					features {
+						text
+						url
+						event {
+							category
+							action
+						}
+					}
+				}
+				carousel {
+					array {
+						img
+						caption
+					}
+				}
+				numbers {
+					heading
+					array {
+						prefix
+						number
+						suffix
+						caption
+					}
+				}
+				ctaLower {
+					img
+					accent
+					heading
+					buttons {
+						text
+						url
+						event {
+							category
+							action
+						}
+					}
+				}
+				menuFeature {
+					heading
+					buttons {
+						text
+						url
+						isSecondary
+					}
+					imgs
+				}
+			}
+		}
+	}
 `;
