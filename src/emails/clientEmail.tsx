f97e9127d0a344c672fd9f0d97700eec92eaa5cb
johnchourajr <hi@john.design>
2023-06-26 23:22:21 -0700

    feat: email update for dark mode

--- src/emails/clientEmail.tsx
+++ src/emails/clientEmail.tsx
@@ -25,18 +25,19 @@ export const ClientEmail = (props: any) => {
       <Preview>{previewText}</Preview>
       <Tailwind>
         <Body className="my-auto mx-auto bg-white font-sans">
-          <Section className="mt-[24px] mb-[16px] w-[465px]">
-            <a href="https://thegrandlb.com" target="_blank" rel="noreferrer">
-              <Img
-                src={`https://images.prismic.io/the-grand/cb6bbe74-9712-4cf9-bec3-145cc675a490_logo.png?auto=compress,format`}
-                width="177"
-                height="65"
-                alt="The Grand"
-                className="my-0"
-              />
-            </a>
-          </Section>
-          <Container className="mx-auto w-[465px] rounded bg-[#FAF2EB] px-[32px] pt-[12px] pb-[32px]">
+          <Container className="mx-auto mt-[24px] w-[465px] rounded bg-[#FAF2EB] px-[32px] pt-[12px] pb-[32px]">
+            <Section className="mt-[24px] mb-[16px] w-[465px]">
+              <a href="https://thegrandlb.com" target="_blank" rel="noreferrer">
+                <Img
+                  src={`https://images.prismic.io/the-grand/cb6bbe74-9712-4cf9-bec3-145cc675a490_logo.png?auto=compress,format`}
+                  width="177"
+                  height="65"
+                  alt="The Grand"
+                  className="logo-light"
+                />
+              </a>
+            </Section>
+            <hr className="border-[1px] !border-solid border-[white] !outline-[none]" />
             <h1 className="text-[20px]">Hey, {full_name.value || "there"}.</h1>
             <p className="text-[16px]">
               Thanks for your submission. Our sales team should reach out in the
--- src/emails/salesEmail.tsx
+++ src/emails/salesEmail.tsx
@@ -1,13 +1,13 @@
 import { FormState } from "@/data/form.types";
 import {
-Body,
-Container,
-Head,
-Html,
-Img,
-Preview,
-Section,
-Tailwind
+  Body,
+  Container,
+  Head,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Tailwind,
 } from "@react-email/components";
 import EmailBody from "../components/email/emailBody";
 
@@ -42,18 +42,19 @@ export const SalesEmail = (props: any) => {
       <Preview>{previewText}</Preview>
       <Tailwind>
         <Body className="my-auto mx-auto bg-white font-sans">
-          <Section className="mt-[24px] mb-[16px] w-[465px]">
-            <a href="https://thegrandlb.com" target="_blank" rel="noreferrer">
-              <Img
-                src={`https://images.prismic.io/the-grand/cb6bbe74-9712-4cf9-bec3-145cc675a490_logo.png?auto=compress,format`}
-                width="177"
-                height="65"
-                alt="The Grand"
-                className="my-0"
-              />
-            </a>
-          </Section>
-          <Container className="mx-auto w-[465px] rounded bg-[#FAF2EB] px-[32px] pt-[12px] pb-[32px]">
+          <Container className="mx-auto mt-[24px] w-[465px] rounded bg-[#FAF2EB] px-[32px] pt-[12px] pb-[32px]">
+            <Section className="mt-[24px] mb-[16px] w-[465px]">
+              <a href="https://thegrandlb.com" target="_blank" rel="noreferrer">
+                <Img
+                  src={`https://images.prismic.io/the-grand/cb6bbe74-9712-4cf9-bec3-145cc675a490_logo.png?auto=compress,format`}
+                  width="177"
+                  height="65"
+                  alt="The Grand"
+                  className="logo-light"
+                />
+              </a>
+            </Section>
+            <hr className="border-[1px] !border-solid border-[white] !outline-[none]" />
             <h1 className="text-[20px]">Hey sales team.</h1>
             <p className="text-[16px]">
               {full_name.value || "Someone"} just submitted an inquiry for{" "}
