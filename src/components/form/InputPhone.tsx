16bca50ab0e31bf043c0192db1c7dc0b040fa24d
johnchourajr <hi@john.design>
2023-08-15 23:22:11 -0700

    feat: updating db addition

--- src/components/form/InputPhone.tsx
+++ src/components/form/InputPhone.tsx
@@ -2,6 +2,38 @@ import { InputProps } from "@/data/form.types";
 import clsx from "clsx";
 import { useState } from "react";
 
+// function to format phone number from 555-555-5555 to (555) 555-5555
+export const formatPhoneForDatabase = (phoneNumberString: string) => {
+  const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  const match = cleaned.match(/^(\d{0,3})(\d{0,3})(\d{0,4})$/);
+  if (match) {
+    return (
+      "(" +
+      match[1] +
+      (match[1] && match[2] ? ") " : "") +
+      match[2] +
+      (match[2] && match[3] ? "-" : "") +
+      match[3]
+    );
+  }
+  return phoneNumberString;
+};
+
+export const formatPhoneNumber = (phoneNumberString: string) => {
+  const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  const match = cleaned.match(/^(\d{0,3})(\d{0,3})(\d{0,4})$/);
+  if (match) {
+    return (
+      match[1] +
+      (match[1] && match[2] ? "-" : "") +
+      match[2] +
+      (match[2] && match[3] ? "-" : "") +
+      match[3]
+    );
+  }
+  return phoneNumberString;
+};
+
 const InputPhone = ({
   name,
   page_key,
@@ -15,21 +47,6 @@ const InputPhone = ({
   ...rest
 }: InputProps) => {
   const nameString = name.toString();
-
-  const formatPhoneNumber = (phoneNumberString: string) => {
-    const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
-    const match = cleaned.match(/^(\d{0,3})(\d{0,3})(\d{0,4})$/);
-    if (match) {
-      return (
-        match[1] +
-        (match[1] && match[2] ? "-" : "") +
-        match[2] +
-        (match[2] && match[3] ? "-" : "") +
-        match[3]
-      );
-    }
-    return phoneNumberString;
-  };
 
   const [formattedValue, setFormattedValue] = useState(
     formatPhoneNumber(value)
--- src/pages/api/add-to-database.ts
+++ src/pages/api/add-to-database.ts
@@ -1,3 +1,5 @@
+import { formatPhoneForDatabase } from "@/components/form/InputPhone";
+import { formatDate } from "@/utils/utils";
 import Airtable from "airtable";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -12,31 +14,37 @@ export default async function handler(
   if (req.method !== "POST") {
     res.status(405).end(); // Method Not Allowed
     return;
-  }
+  } else {
+    // Your form data from the request body
+    const { phone, desired_date, ...formData } = req.body;
+    const formattedPhone = formatPhoneForDatabase(phone);
+    const formattedDate = formatDate(desired_date);
 
-  // Your form data from the request body
-  const formData = req.body;
+    try {
+      const table = "submissions";
 
-  try {
-    const table = "submissions";
+      // Map form data to fields
+      const fields = {
+        // format phone from 555-555-5555 to (555) 555-5555
+        phone: formattedPhone,
+        desired_date: formattedDate,
+        ...formData,
+      };
 
-    // Map form data to fields
-    const fields = {
-      ...formData,
-    };
+      // Create a new record in Airtable
+      const createdRecord = await base(table).create([{ fields }]);
 
-    // Create a new record in Airtable
-    const createdRecord = await base(table).create([{ fields }]);
+      res.status(200).json({
+        message: "Record created successfully",
+        record: createdRecord,
+      });
 
-    res
-      .status(200)
-      .json({ message: "Record created successfully", record: createdRecord });
-
-    res.status(200).json({ message: "Record created successfully" });
-  } catch (error: any) {
-    console.error("Error:", error);
-    res
-      .status(500)
-      .json({ message: "An error occurred", error: error.message });
+      res.status(200).json({ message: "Record created successfully" });
+    } catch (error: any) {
+      console.error("Error:", error);
+      res
+        .status(500)
+        .json({ message: "An error occurred", error: error.message });
+    }
   }
 }
