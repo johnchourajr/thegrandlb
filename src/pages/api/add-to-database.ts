cf87ba127ff436ba7c9fcbbd7c12b9e4fe5e45be
johnchourajr <hi@john.design>
2023-09-06 21:58:12 -0700

    feat: adding headcount

--- src/pages/api/add-to-database.ts
+++ src/pages/api/add-to-database.ts
@@ -27,7 +27,7 @@ export default async function handler(
         ...formData,
       };
 
-      const sqlCommand = `INSERT INTO ${TABLE} (full_name, email, phone, event_name, event_type, desired_date, desired_time, desired_space, additional_details, created_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
+      const sqlCommand = `INSERT INTO ${TABLE} (full_name, email, phone, event_name, event_type, head_count, desired_date, desired_time, desired_space, additional_details, created_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
 
       await db.query(sqlCommand, [
         fields.full_name,
@@ -35,6 +35,7 @@ export default async function handler(
         fields.phone,
         fields.event_name,
         fields.event_type,
+        fields.head_count,
         fields.desired_date,
         fields.desired_time,
         fields.desired_space,
