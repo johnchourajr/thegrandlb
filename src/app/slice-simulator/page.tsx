b842f22e6e71e520bce545c2ed929e3b0598c2be
johnchourajr <hi@john.design>
2026-02-13 00:25:08 -0800

    Fix slice preview

---
+++ src/app/slice-simulator/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
+import { SliceZone } from "@prismicio/react";
+import { components } from "../../../slices";
+
+export default function SliceSimulatorPage() {
+  return (
+    <SliceSimulator
+      sliceZone={({ slices }) => (
+        <SliceZone slices={slices} components={components} />
+      )}
+    />
+  );
+}
